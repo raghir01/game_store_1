b27a52ba3e5f2b08198b0d58cda7b794e0e412bb
Raghi Ramachandran <raghir01@gmail.com>
2019-09-05 21:28:48 -0400

    Refactored application name

--- src/main/java/com/company/RaghiRamachandranU1Capstone/GameStoreApplication.java
+++ src/main/java/com/company/RaghiRamachandranU1Capstone/GameStoreApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RaghiRamachandranU1CapstoneApplication {
+public class GameStoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RaghiRamachandranU1CapstoneApplication.class, args);
+		SpringApplication.run(GameStoreApplication.class, args);
 	}
 
 }
